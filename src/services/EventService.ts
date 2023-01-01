9caa2f0f5b4238e73e73e890d4dfa6fdbe12c6c1
wansoo <jwansoo@hanmail.net>
2023-01-01 18:44:50 +0900

    eventservice

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Touring-Vue-Router',
+  baseURL: 'https://my-json-server.typicode.com/jwansoo/touring_vuerouter_comp_ts',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
